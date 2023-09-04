849ced557cb38fc3e7a41168db7e18238213e25a
alpha951 <20uec068@lnmiit.ac.in>
2023-09-04 11:44:31 +0530

    Added seats modes associated to airplane

--- src/models/airplane.js
+++ src/models/airplane.js
@@ -13,6 +13,11 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: "airplaneId",
         onDelete: "CASCADE",
       });
+
+      this.hasMany(models.Seat, {
+        foreignKey: "airplaneId",
+        onDelete: "CASCADE",
+      });
     }
   }
   Airplane.init(
---
+++ src/utils/common/enums.js
@@ -0,0 +1,8 @@
+const SEAT_TYPE = {
+  BUISNESS: "buisness",
+  ECONOMY: "economy",
+  PREMIUM_ECONOMY: "premium_economy",
+  FIRST_CLASS: "first_class",
+};
+
+module.exports = { SEAT_TYPE };
