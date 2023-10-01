75a5e86b7f7ee118034c35f989b12b9c81c1dc93
alpha951 <20uec068@lnmiit.ac.in>
2023-10-01 22:00:32 +0530

    comments removed

--- API_GATEWAY/src/middlewares/auth-request-middleware.js
+++ API_GATEWAY/src/middlewares/auth-request-middleware.js
@@ -48,6 +48,8 @@ async function checkAuth(req, res, next) {
     // console.log("Response from checkAtuh :", response);
     if (response) {
       req.user = response; // setting the user id in the req object
+      req.headers["user-info"] = response;
+      console.log("user-info", req.headers["user-info"]);
       console.log("req.user printing", req.user);
       next();
     }
